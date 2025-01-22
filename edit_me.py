a3bb0d88786de3f81e3b85be9e2937d575413118
Jude <judebrooks2023@gmail.com>
2025-01-21 18:54:34 -0600

    doing the lab 1
    i did the lab one :)

---
+++ edit_me.py
@@ -0,0 +1,16 @@
+#Jude Brooks
+#Lab 1
+
+def find_max(lst):
+    """return the maximum element"""
+
+    current_max = lst[0]
+    for i in range(len(lst)):
+        if lst[i] > current_max:
+            current_max = lst[i]
+
+    return current_max
+
+test_list = [2112*i % 2024 for i in range(10000)]
+
+print(find_max(test_list))
